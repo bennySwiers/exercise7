0bcc5f7929b85527055a657821e76a8b8d10a3f4
junkoliver <junkoliver@gmx.de>
2014-12-09 15:07:16 +0100

    made it a webshit

---
+++ src/main/java/CrawlServlet.java
@@ -0,0 +1,47 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Arte on 08.12.2014.
+ */
+@SuppressWarnings("serial")
+public class CrawlServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<String> test = new ArrayList<String>();
+        test.add("www.heise.de");
+        test.add("www.nba.com");
+        test.add("www.chip.de");
+        test.add("www.bla.de");
+
+        StringBuilder JSON = new StringBuilder();
+
+        JSON.append("{\n");
+        JSON.append("\"name\":\"\",\n");
+        JSON.append("\"children\":[\n");
+
+        for(String s : test){
+            JSON.append("\t {\n");
+            JSON.append("\t\t \"name\":\"cluster " + s + "\",\n");
+            JSON.append("\t\t \"children\":[\n");
+            JSON.append("\t\t\t {\"name\":\"" + s +"\"}\n");
+
+            JSON.append("\t\t ]\n");
+            JSON.append("\t },\n");
+        }JSON.deleteCharAt(JSON.toString().length() -2);
+        JSON.append("]\n");
+        JSON.append("}\n");
+
+
+        resp.setContentType("text/json");
+        resp.getWriter().write(JSON.toString());
+        System.out.println(JSON.toString());
+    }
+}
